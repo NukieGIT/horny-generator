87dcc7b4113081b6a745e1f1153b0392f7d1e895
Nukie <92524294+NukieGIT@users.noreply.github.com>
2023-08-26 22:16:18 +0200

    feat: tabs can be a renderless component
    Co-authored-by: Radziu47 <Radziu47@users.noreply.github.com>

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -2,32 +2,39 @@
 
 <div class="container">
     <div class="tabs">
-        <slot :tabTitles="tabTitles">
-            <div v-for="tab in tabs" @click="currentTab = tab.component" class="tab" :class="{ selected: tab.component === currentTab }">
-                {{ tab.title }}
+        <slot :methods="{ switchCurrentTab }" :currentTab="getCurrentTab">
+            <div v-for="tab in tabs" @click="switchCurrentTab(tab)" class="tab" :class="{ selected: tab.uuid === currentTab.uuid }">
+                {{ tab.item.title }}
             </div>
         </slot>
     </div>
     <div class="view">
-        <component :is="currentTab" />
+        <KeepAlive>
+            <component :is="currentTab.item.component" />
+        </KeepAlive>
     </div>
 </div>
 
 </template>
 
 <script setup lang="ts">
-import { computed, ref, shallowRef } from 'vue';
+import { computed, shallowRef } from 'vue';
 import type { TabComponent } from '@/components/Tabs/TabsTypes';
+import type { IUUIDItem } from '@/ts/utils/utilityFuncs';
 
 const props = defineProps<{
-    tabs: TabComponent[];
+    tabs: IUUIDItem<TabComponent>[];
 }>();
 
-const tabTitles = computed(() => {
-    return props.tabs.map(tab => tab.title);
-})
+function switchCurrentTab(tab: IUUIDItem<TabComponent>) {
+    currentTab.value = tab;
+}
 
-const currentTab = shallowRef(props.tabs[0].component);
+const currentTab = shallowRef(props.tabs[0]);
+
+const getCurrentTab = computed(() => {
+    return currentTab.value;
+})
 
 </script>
 
@@ -48,6 +55,7 @@ const currentTab = shallowRef(props.tabs[0].component);
 }
 
 .tab {
+    cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
